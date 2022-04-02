939a34e66153d5b602e3ce5ccd4807a7ceabbd4e
OutsiderLost <87493359+OutsiderLost@users.noreply.github.com>
2022-04-02 09:57:52 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,11 +3,21 @@
 chmod +x *.sh
 echo " "
 echo "move file: gitscript-allin.sh -> /usr/bin/gitscript"
-# echo "copy file: gitscript-allin.sh -> /usr/bin/gitscript"
+## echo "copy file: gitscript-allin.sh -> /usr/bin/gitscript"
 echo " "
 sleep 1
-[ -f gitscript-allin.sh ] && mv gitscript-allin.sh /usr/bin/gitscript || echo -e 'error, no such file (gitscript-allin.sh) !!!\n'
-# [ -f gitscript-allin.sh ] && cp gitscript-allin.sh /usr/bin/gitscript || echo -e 'error, no such file (gitscript-allin.sh) !!!\n'
+### FILE MOVE ###
+[ -f gitscript-allin.sh ] && mv gitscript-allin.sh /usr/bin/gitscript
+## [ -f gitscript-allin.sh ] && cp gitscript-allin.sh /usr/bin/gitscript
+if [ "$?" = '0' ]; then
+  echo -n ""
+else
+  echo 'error, no such file (gitscript-allin.sh) !!!'
+  echo " "
+  echo "(exit...)"
+  echo " "
+  exit 
+fi
 echo "use -> gitscript {options}or{{input1}or/and{input2}}"
 echo " "
 echo "test -> gitscript --help"
@@ -15,4 +25,17 @@ echo " "
 sleep 1
 echo "if subtitle under displayed -> install OK"
 echo " "
-[ -f /usr/bin/gitscript ] && gitscript --help || echo -e 'error, there is something wrong...!!!\n'
+[ -f /usr/bin/gitscript ] && gitscript --help
+if [ "$?" = '0' ]; then
+  ### ONLY COPY METHOD => NO NEED ###
+  rm -r ../gitscript
+  cd /root/
+  echo " "
+  echo "Success! removed folder, exit the place -> cd /root/"
+else
+  echo 'error, there is something wrong...!!!'
+  echo " "
+  echo "(exit...)"
+  echo " "
+  exit 
+fi
